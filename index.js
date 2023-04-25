2f873079cfa4761fbbb515bdd0fd8023f6e6c643
Nguyen Vy <nttvy27@gmail.com>
2023-04-25 09:47:17 +0700

    Handle Game Over

--- index.js
+++ index.js
@@ -22,3 +22,9 @@ const updateFoodPosition = () => {
   foodX = Math.floor(Math.random() * 30) + 1;
   foodY = Math.floor(Math.random() * 30) + 1;
 };
+
+const handleGameOver = () => {
+  clearInterval(setIntervalId);
+  alert("Game Over! Press OK to replay...");
+  location.reload();
+};
