1c09755b71e71e5ca423d950e4932c074db46058
Nguyen Vy <nttvy27@gmail.com>
2023-04-25 11:52:19 +0700

    Call functions

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ let snakeX = 5,
   snakeY = 5;
 let velocityX = 0,
   velocityY = 0;
-let sankeBody = [];
+let snakeBody = [];
 let setIntervalId;
 let score = 0;
 
@@ -36,7 +36,7 @@ const changeDirection = (e) => {
     velocityY = -1;
   } else if (e.key === "ArrowDown" && velocityY != -1) {
     velocityX = 0;
-    velocityY = -1;
+    velocityY = 1;
   } else if (e.key === "ArrowLeft" && velocityX != 1) {
     velocityX = -1;
     velocityY = 0;
@@ -54,3 +54,54 @@ controls.forEach((button) =>
     })
   )
 );
+
+const initGame = () => {
+  if (gameOver) return handleGameOver();
+  let html = `<div class="food" style="grid-area: ${foodY} / ${foodX}"></div>`;
+
+  // When snake eat food
+  if (snakeX === foodX && snakeY === foodY) {
+    updateFoodPosition();
+    snakeBody.push([foodY, foodX]); // Add food to snake body array
+    score++;
+    highScore = score >= highScore ? score : highScore; // If score > high score => high score = score
+
+    localStorage.setItem("high-score", highScore);
+    scoreElement.innerText = `Score: ${score}`;
+    highScoreElement.innerText = `High Score: ${highScore}`;
+  }
+
+  // Update Sanke Head
+  snakeX += velocityX;
+  snakeY += velocityY;
+
+  // Shifthing forward values of elements in snake boby by one
+  for (let i = snakeBody.length - 1; i > 0; i--) {
+    snakeBody[i] = snakeBody[i - 1];
+  }
+  snakeBody[0] = [snakeX, snakeY];
+
+  // Check snake body is out of wall or not
+  if (snakeX <= 0 || snakeX > 30 || snakeY <= 0 || snakeY > 30) {
+    return (gameOver = true);
+  }
+
+  // Add div for each part of snake body
+  for (let i = 0; i < snakeBody.length; i++) {
+    html += `<div class="head" style="grid-area: ${snakeBody[i][1]} / ${snakeBody[i][0]}"}></div>`;
+    // Check snake hit body or no
+    if (
+      i !== 0 &&
+      snakeBody[0][1] === snakeBody[i][1] &&
+      snakeBody[0][0] === snakeBody[i][0]
+    ) {
+      gameOver = true;
+    }
+  }
+
+  playBoard.innerHTML = html;
+};
+
+updateFoodPosition();
+setIntervalId = setInterval(initGame, 200);
+document.addEventListener("keyup", changeDirection);
